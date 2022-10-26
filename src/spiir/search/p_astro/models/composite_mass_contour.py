ebadcb62d87233ae9dfa7cad3155713aaeb5c8c0
Daniel Tang <tang.d@hotmail.com>
2022-10-26 18:21:58 +1100

    run black

--- src/spiir/search/p_astro/models/composite_mass_contour.py
+++ src/spiir/search/p_astro/models/composite_mass_contour.py
@@ -13,15 +13,12 @@ class FGMCMassContourModel:
     def __init__(
         self,
         fgmc_model: fgmc.TwoComponentFGMCToyModel,
-        mass_contour_model: Optional[mass_contour.MassContourModel]=None,
+        mass_contour_model: Optional[mass_contour.MassContourModel] = None,
     ):
         # to do: intiailise from config file(?)
         self.fgmc_model = fgmc_model
         self.mass_contour_model = mass_contour_model or mass_contour.MassContourModel(
-            a0=0.7598851608618243,
-            b0=0.6849582413938586,
-            b1=0.4668975492108711,
-            m0=0.01
+            a0=0.7598851608618243, b0=0.6849582413938586, b1=0.4668975492108711, m0=0.01
         )
 
     def predict(
@@ -33,6 +30,6 @@ class FGMCMassContourModel:
     ):
         astro_prob = self.fgmc_model.predict(far, snr)
         source_probs = self.mass_contour_model.predict(mchirp, snr, eff_dist)
-        probs = {key: source_probs[key]*astro_prob for key in source_probs}
+        probs = {key: source_probs[key] * astro_prob for key in source_probs}
         probs.update({"Terrestrial": 1 - astro_prob})
         return probs
